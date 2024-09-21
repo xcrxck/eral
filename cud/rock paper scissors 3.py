09818ceb8ec94fc4c17ebe4a6dccc07df55f6885
Erfan Moayedi <imhappy4590@gmail.com>
2024-09-21 21:03:58 +0330

    Add files via upload

--- cud/rock paper scissors 3.py
+++ cud/rock paper scissors 3.py
@@ -15,26 +15,29 @@ print('Welcome to Rock, Paper, Scissors!')
 
 time.sleep(1)
 
-# Loop for setting game speed
-delayTime = 0
-def game_speed():
+# Function for setting game speed
+def game_speed():  
+ global delayTime
+ 
+ delayTime = 0
+ 
  while True: 
      setDelay = str.lower(input(f'Type your game speed ({red}Slow{reset}/{green}Normal{reset}/{blue}Fast{reset}): '))
 
      if setDelay == 'slow':
          delayTime = 3
-         break
-    
+
      elif setDelay == 'normal':
          delayTime = 1.5
-         break
     
      elif setDelay == 'fast':
          delayTime = 0.5
-         break
-     
+         
      else:
          print('Wrong Input! Please try again')
+
+     if delayTime != 0:
+         break
 game_speed()
          
 # Score counters
@@ -42,11 +45,12 @@ userScore = 0
 cpuScore = 0
 
 # Main Game loop
-while True: 
+status = True
+while status == True: 
  allChoices = ['Rock','Paper','Scissors']
  
  # User input to lowercase allowing for various capitalized or uncapitalized inputs also shows scores every round
- userChoice = str.lower(input(f'(Current scores: {red}Cpu: {cpuScore} {blue}User: {userScore}{reset}) {yellow}Play your {italic}turn or type "P" for pause menu:{reset} '))
+ userChoice = str.lower(input(f'{yellow}Play your {italic}turn or type "P" for pause menu:{reset} '))
 
  # Similar thing for cpuChoice. CPU chooses a random item from allChoices
  cpuChoice = str.lower(random.choice(allChoices))
@@ -55,10 +59,12 @@ while True:
  
  # Outcome for a draw
  if cpuChoice == userChoice:
+     
      result = 'draw'
 
  # Outcome if user has chosen paper
  elif userChoice == 'paper':
+     
      if cpuChoice == 'rock':
          result = 'win'
          
@@ -67,6 +73,7 @@ while True:
          
  # Outcome if user has chosen scissors       
  elif userChoice == 'scissors':
+     
      if cpuChoice == 'paper':
          result = 'win'
          
@@ -75,6 +82,7 @@ while True:
          
  # Outcome if user has chosen rock      
  elif userChoice == 'rock':
+     
      if cpuChoice == 'scissors':
          result = 'win'
          
@@ -85,19 +93,35 @@ while True:
  elif userChoice == 'p' or userChoice == 'pause':
      print('Game paused')
      
-     pause = str.lower(input('Type "Q" to exit the game or type "R" to reset scores or type "GS" to change the game speed, otherwise press enter key to unpause: '))
+     # Loop for pause menu
+     while True:
+         pause = str.lower(input(f'Type {red}{bold}"Q"{reset} to exit the game/{yellow}{bold}"R"{reset} to reset scores/{green}{bold}"GS"{reset} to change the game speed, otherwise press {blue}{bold}enter key{reset} to unpause: '))
      
-     if pause == 'q' or pause == 'quit':
-         break
+         if pause == 'q' or pause == 'quit':
+             status = False
+             break
      
-     elif pause == 'r' or pause == 'reset':
-         cpuScore = 0
-         userScore = 0
+         elif pause == 'r' or pause == 'reset':
+             cpuScore = 0
+             userScore = 0
+             
+             print('Changes applied!')
+             
+             time.sleep(1)
          
-     elif pause == 'gs' or pause == 'speed' or pause == 's' or pause == 'change speed':
-         game_speed()
+         elif pause == 'gs' or pause == 'gamespeed' or pause == 's' or pause == 'change speed':
+             game_speed()
+             
+             print('Changes applied!')
+             
+             time.sleep(1)
+             
+         elif pause == '':
+             break
          
-
+         else:
+             print('Wrong input! Please try again')
+         
  else: 
      print(f'Wrong input! Please choose between "{darkGrey}Rock{reset}, Paper,{red} Scissors{reset}".')
      
@@ -121,3 +145,9 @@ while True:
      cpuScore += 1
      
      print(f'{red}CPU {bold}win!{reset}')
+     
+ # Shows score (has the same conditions as line 127 but difference is when they happen)    
+ if result != '':
+     time.sleep(delayTime/2)
+     
+     print(f'Current scores: {red}Cpu: {cpuScore} {blue}User: {userScore}{reset}')
